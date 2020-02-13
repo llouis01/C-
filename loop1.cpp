fa036d31972094b3a6d3b3c1e0512646041faca1
Loubens Louis <53797568+llouis01@users.noreply.github.com>
2020-02-12 19:34:07 -0500

    Add files via upload
    if and else intro code in C++

---
+++ loop1.cpp
@@ -0,0 +1,21 @@
+// playing with if and else statements in C++
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int x = 0;
+	cout<<"Ent an int: "<<endl;
+	cin >> x;
+
+	if (x < 17) {
+	cout<<"Make it higher.."<<endl;
+	} else if (x > 17) {
+	cout<<"Make it lower.."<<endl;
+	} else if (x == 17) {
+	cout<<"Jackpot!"<<endl;
+	}
+
+return 0;
+}
