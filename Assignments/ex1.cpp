369b183c151d81b731595932278b7e1da3c53047
llouis01 <loubenslouis@live.com>
2020-05-04 21:49:07 -0400

    Working on Q2
    Program runs, but returns unexpected results

--- Assignments/ex1.cpp
+++ Assignments/ex1.cpp
@@ -3,16 +3,18 @@
 #include <iostream>
 using namespace std;
 
-int fact(x);
+int n, k = 0;
+
+int fact(int x);
 
 int perm(int n, int k);
 
-int n, k = 0;
 
 int main(){
 
 cout<<"Enter two numbers for permutation: \n";
-cin>>n,k;
+cin>>n>>k;
+// cin>>k;
 
 cout<<"Permutation yields: "<<perm(n, k);
 cout<<endl;
@@ -22,15 +24,16 @@ return 0;
 
 int fact(int x){
 
-	if (x == 1){
-		return 1;
-	else
-		return x*(x-1);
+	while (x > 1){
+		if (x == 1){
+			return 1;
+		} else
+			return x*(x-1);
 	}
 }
 
 
 int perm(int n, int k){
 
-	fact(n)/fact(n-k);
+	return fact(n)/fact(n-k);
 }
