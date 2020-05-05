c19422e355e2642feafd683f42f4fb1b98722691
llouis01 <loubenslouis@live.com>
2020-05-04 22:26:00 -0400

    Renamed ex1 as Q2 for HW4

--- Assignments/Q2.cpp
+++ Assignments/Q2.cpp
@@ -1,8 +1,34 @@
+//Loubens Louis, Q2
+
 #include <iostream>
 using namespace std;
 
+int n, k= 0;
+
+int fact(int x){
+
+	while (x > 1){
+		if (x == 1){
+		return 1;
+		} else
+		return x * fact(x-1);
+	}
+}
 
 
 int perm(int n, int k){
 
-	
+	return fact(n) / fact((n-k));
+}
+
+
+int main(){
+
+cout<<"Enter two numbers for permutation: \n";
+cin>>n>>k;
+
+cout<<"Permutation yields: "<<perm(n, k); // n=5 & k=2 should yield 20
+cout<<endl;
+
+return 0;
+}
