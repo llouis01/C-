8ce55d148a61d16b1c4db1f63dea2c754bfb1c06
llouis01 <loubenslouis@live.com>
2020-05-04 20:42:33 -0400

    Completed Q1 of HW4

--- Assignments/untitled2.cpp
+++ Assignments/untitled2.cpp
@@ -1,15 +1,19 @@
+// Loubens Louis, Q1_HW4
 #include <iostream>
 using namespace std;
 
+int digit(int n, int k) {
+for (int i = 0; i < k; i++) {
+n /= 10;
+}
+return n % 10;
+}
 
-int digit(int n, int k){
+int main() {
+int n = 29415;
+cout << "digit(n, 0): " << digit(n, 0) << endl;
+cout << "digit(n, 2): " << digit(n, 2);
+return 0;
+}
 
-	for (int i = 0; i < counter; i++){
-		arr[i] = i;
-	}
-	
-
-int main(){
-
-	
 	
