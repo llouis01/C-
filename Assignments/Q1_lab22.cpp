48ad1076a481678be32be827e5ec2bdde8d9f27e
llouis01 <loubenslouis@live.com>
2020-05-05 19:01:10 -0400

    Complete
    1) the input is displayed in both the top and below the output...why?

--- Assignments/Q1_lab22.cpp
+++ Assignments/Q1_lab22.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int row = 0;
-cout<<"Enter size of row: "<<endl;
-cin>>row;
-cout<<endl;
-
 int rectangle(int row){
 
 	for (int r = 0; r < row; r++){
 		for (int c = 0; c <= 10 ; c++){
-			if (r==c) cout<<"*";
+			if (r==c || r<=c || r>=c) cout<<"*";
 	}
 	cout<<endl;
 	}
 }
+
+
+int main() {
+
+int row = 0;
+cout<<"Enter size of row: ";
+cin>>row;
+cout<<endl;
+
+cout<<rectangle(row)<<endl;
+
+return 0;
+}
+
