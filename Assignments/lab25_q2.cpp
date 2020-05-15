2c7a4b686463b5292b4b7a4bd6a61e0147a06b6a
llouis01 <loubenslouis@live.com>
2020-05-14 20:40:28 -0400

    Lab25_q2
    1) demonstrate the use of argv in the command line

---
+++ Assignments/lab25_q2.cpp
@@ -0,0 +1,13 @@
+// Loubens Louis, Lab 25, xtra cred.
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]){
+
+string str1 = argv[1], str2 = argv[2];
+
+cout<<"\nThe result is \""<<str1+str2
+    <<"\"."<<endl;
+
+return 0;
+}
